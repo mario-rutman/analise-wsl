5da6edf5c7811a0508678dd13db7524d7c756b83
mario-rutman <marrut@gmail.com>
2021-01-10 15:53:27 -0300

    Função para extrair os dados das competições.

---
+++ wsl_funcao.R
@@ -0,0 +1,85 @@
+# O trabalho para fazer um site com analises dos resultados dos campeonatos da 
+# World Surf League terá 3 etapas: a raspagem dos dados; a análise; e, a apresentação
+# no Shiny.
+# Agora farei a raspagem dos dados encontrados no site. Será uma raspagem meio no braço,
+# pois ainda estou conhecendo o pacote rvest.
+
+
+
+# Vou usar o pacote “rvest”.
+library(tidyverse)
+library(rvest)
+library(dplyr)
+library(robotstxt)
+library(xml2)
+library(purrr)
+library(lubridate)
+library(tibble)
+
+# Cada round tem um id diferente na url.
+# id_round <- c(16,36,20,42,48,69,71)
+
+# Cada phase, round (quartas, semi, final etc) é um url diferente.
+
+
+  func_etapas <- function(roundID, num_heat, athlete_per_heat, quart_semi_final,
+                          ano, periodo,
+                          lugar, evento, genero) {
+    # Criando a coluna score.
+      vju <- paste0('https://www.worldsurfleague.com/events/2008/mct/4/quiksilver-pro-gold-coast?roundId=',roundID)
+      score <- read_html(vju)%>%
+        html_nodes(".wave-wrap .score") %>%
+        html_text() %>%
+        str_squish()
+    
+    # Criando a coluna athlete.
+      kol <- paste0('https://www.worldsurfleague.com/events/2008/mct/4/quiksilver-pro-gold-coast?roundId=',roundID)
+      athlete <- read_html(kol)%>%
+        html_nodes(".avatar--athlete") %>%
+        html_text() %>%
+        str_squish() %>% 
+        rep(each = 15)
+    
+    # Criando a coluna heat.
+      heat <- rep(c(1:num_heat), each = athlete_per_heat * 15)
+    
+      # Número de linhas do tibble.
+    num_lin <- length(athlete)
+    
+    # Coluna year
+    year <- rep(ano, num_lin)
+    
+    # Coluna when (quando foi o campeonato)
+    when <- rep(periodo, num_lin)
+    
+    # Local do campeonato.
+    local <- rep(lugar,num_lin)
+    
+    # Cammpeonato masculino ou feminino (Mens, Womens)
+    gender <- rep(genero, num_lin)
+    
+    # Etapa do campeonato.
+    stop <- rep(1, num_lin)
+    
+    # Nome do campeonato.
+    event <- rep(evento, num_lin)
+    
+    # Round significa quartas, semi, final etc.
+    round <- rep(quart_semi_final, num_lin)
+    
+    # É o número da onda do competidor.
+    wave <- rep(1:15, num_lin/15)
+    
+    # Criando o tibble e salvando.
+    tabela <- tibble(year, when, local, gender, event, stop,
+                       round, heat, athlete, wave, score) %>%
+      # Excluindo as células de score vazias. 
+      filter(score != "") 
+    write_rds(tabela, "tabela.rds")
+  }
+
+func_etapas(roundID = 71, num_heat = 1, athlete_per_heat = 2, 
+            quart_semi_final = "Final", ano = 2008,
+            periodo = "Feb 23 - May 5, 2008", 
+            lugar = "Gold Coast, Queensland, Australia",
+            evento = "Quiksilver Pro Gold Coast", genero = "Men's")
